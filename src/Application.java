51b47afba0cd37e73850173648676015d9821a33
andreydove <andreydove@gmail.com>
2021-05-31 21:14:46 +0300

    lesson3 homework

---
+++ src/Application.java
@@ -0,0 +1,5 @@
+public class Application {
+    public static void main(String[] args){
+        Lesson3_2.printArray(Lesson3.returnList(Lesson3.arr));
+    }
+}
---
+++ src/Lesson3_2.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lesson3_2 {
+    public static void printArray(List<String> list){
+        for(int i=0; i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
+    }
+}
