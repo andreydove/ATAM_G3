3dbd04044ea9763bf52c3d7d51ba8633b7c576e6
andreydove <andreydove@gmail.com>
2021-06-07 17:26:01 +0300

    second homework

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args){
-        Lesson3_2.printArray(Lesson3.returnList(Lesson3.arr));
+        //Lesson3_2.printArray(Lesson3.returnList(Lesson3.arr));
+        //second homework below
+        Lesson3_2.printArrayUsingStream(null);
+        Lesson3_2.printArrayUsingStream(Lesson3.returnList(Lesson3.arr));
+        Lesson3_2.printArrayUsingStream(new ArrayList<String>());
     }
 }
--- src/Lesson3_2.java
+++ src/Lesson3_2.java
@@ -1,10 +1,24 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lesson3_2 {
-    public static void printArray(List<String> list){
-        for(int i=0; i < list.size(); i++){
+    public static void printArray(List<String> list) {
+        for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
+        }
+    }
+
+    public static void printArrayUsingStream(List<String> list) {
+        try {
+            (list.stream().map(item -> item.split("")).collect(Collectors.toList()))
+                    .stream().forEach(el -> Arrays.stream(el).forEach(System.out::print));
+        }catch (NullPointerException n){
+            System.out.println("Массив не определён. Пожалуйста проверте корректность вводимых значений");
+        }catch (Throwable a){
+            System.out.println("Произошла неизвесная ошибка. Подробнее: " + a.getMessage());
         }
     }
 }
